a5286a1e95ced2c1b02d2327b0b2ca9103309d39
Adelson Guimarães <adelsonguimaraes@gmail.com>
2019-08-12 19:39:23 -0400

    .

--- config.js
+++ config.js
@@ -7,6 +7,48 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
     });
 
     $stateProvider
+        .state('login', {
+            url: "/login",
+            templateUrl: "app/views/login.html",
+            controller: "loginCtrl",
+            data: { pageTitle: 'login', specialClass: 'gray-bg' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['libs/js/plugins/footable/footable.all.min.js', 'libs/css/plugins/footable/footable.core.css']
+                        },
+                        {
+                            name: 'ui.footable',
+                            files: ['libs/js/plugins/footable/angular-footable.js']
+                        },
+                        {
+                            files: ['libs/js/plugins/sweetalert/sweetalert.min.js', 'libs/css/plugins/sweetalert/sweetalert.css']
+                        },
+                        {
+                            name: 'oitozero.ngSweetAlert',
+                            files: ['libs/js/plugins/sweetalert/angular-sweetalert.min.js']
+                        },
+                        {
+                            files: ['libs/css/plugins/iCheck/custom.css', 'libs/js/plugins/iCheck/icheck.min.js']
+                        },
+                        {
+                            files: ['libs/js/plugins/moment/moment.min.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['libs/css/plugins/datapicker/angular-datapicker.css', 'libs/js/plugins/datapicker/angular-datepicker.js']
+                        },
+                        {
+                            files: ['libs/js/plugins/jasny/jasny-bootstrap.min.js']
+                        }
+                    ]);
+
+                }
+            }
+        })
+
+    $stateProvider
         .state('home', {
             url: "/home",
             templateUrl: "app/views/home.html",
